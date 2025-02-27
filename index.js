c3c298ac43ba3638ee1db4f002b5cb01e53b3a98
AmâncioChiluvane <109949333+amanciochiluvane@users.noreply.github.com>
2025-02-27 16:53:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const io = new Server(server, { cors: { origin: "*" } });
 
 let rooms = {};
 app.get("/", (req, res) => {
-    res.send("Servidor está rodando no Render!");
+    res.send("Servidor está rodando no Render Actualizado!");
 });
 
 const receivedFiles = {}; // Para armazenar chunks temporariamente
@@ -17,17 +17,17 @@ const receivedFiles = {}; // Para armazenar chunks temporariamente
 io.on("connection", (socket) => {
     console.log("Novo usuário conectado:", socket.id);
 
-    socket.on("create-room", (callback) => {
+    socket.on("create-room", (username,callback) => {
         const roomId = Math.random().toString(36).substring(2, 8);
-        rooms[roomId] = [socket.id];
+        rooms[roomId] = [ {id: socket.id, username }];
         socket.join(roomId);
         callback(roomId);
         io.to(roomId).emit("update-users", rooms[roomId]);
     });
 
-    socket.on("join-room", (roomId, callback) => {
+    socket.on("join-room", ({ roomId, username }, callback) => {
         if (rooms[roomId]) {
-            rooms[roomId].push(socket.id);
+            rooms[roomId].push({ id: socket.id, username });
             socket.join(roomId);
             callback(true);
             io.to(roomId).emit("update-users", rooms[roomId]);
@@ -66,7 +66,7 @@ io.on("connection", (socket) => {
 
     socket.on("disconnect", () => {
         for (const roomId in rooms) {
-            rooms[roomId] = rooms[roomId].filter((id) => id !== socket.id);
+            rooms[roomId] = rooms[roomId].filter(user => user.id !== socket.id);
             if (rooms[roomId].length === 0) {
                 delete rooms[roomId];
             } else {
@@ -79,4 +79,5 @@ io.on("connection", (socket) => {
 server.listen(5000, () => {
     console.log("Servidor rodando na porta 5000");
 });
+
 
