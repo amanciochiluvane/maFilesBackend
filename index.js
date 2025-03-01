194f5148d89ac9f4f63fcd4fc5c5781c2e564165
AmâncioChiluvane <109949333+amanciochiluvane@users.noreply.github.com>
2025-03-01 12:01:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
+const io = new Server(server, { 
+    cors: { origin: "*" }, 
+    pingTimeout: 60000,  
+    pingInterval: 25000  
+});
+
 
 let rooms = {};
 app.get("/", (req, res) => {
-    res.send("Servidor está rodando no Render Actualizado!");
+    res.send("Servidor está rodando no Vercel!");
 });
 
 const receivedFiles = {}; // Para armazenar chunks temporariamente
@@ -35,6 +40,11 @@ io.on("connection", (socket) => {
             callback(false);
         }
     });
+
+    socket.on("keep-alive", () => {
+        console.log(`Mantendo ${socket.id} ativo`);
+    });
+    
 
     socket.on("send-file-chunk", ({ roomId, chunk, fileName, last, senderId }) => {
         if (!receivedFiles[roomId]) {
@@ -79,5 +89,3 @@ io.on("connection", (socket) => {
 server.listen(5000, () => {
     console.log("Servidor rodando na porta 5000");
 });
-
-
