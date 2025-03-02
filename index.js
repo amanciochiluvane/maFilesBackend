af0e9aa0c83dab7499145bb678eb1f2e9a7c8036
AmâncioChiluvane <109949333+amanciochiluvane@users.noreply.github.com>
2025-03-02 23:09:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const cloudinary = require('cloudinary').v2;
+
+
+cloudinary.config({
+    cloud_name: 'dvetcuxac',
+    api_key: '269513831462676',
+    api_secret: 'TNl7rqCShvXkpToTFm5lA2n3WJs'
+});
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { 
-    cors: { origin: "*" },
-    maxHttpBufferSize: 1e8, // 100MB
-    pingTimeout: 120000,  
+    cors: { origin: "*" }, 
+    pingTimeout: 60000,  
     pingInterval: 25000  
 });
 
-
 let rooms = {};
 app.get("/", (req, res) => {
-    res.send("Servidor está rodando no Railway3!");
+    res.send("Servidor está rodando no Vercel!");
 });
 
 const receivedFiles = {}; // Para armazenar chunks temporariamente
 
-
 io.on("connection", (socket) => {
     console.log("Novo usuário conectado:", socket.id);
 
-    socket.on("create-room", (username,callback) => {
+    // Criação de sala
+    socket.on("create-room", (username, callback) => {
         const roomId = Math.random().toString(36).substring(2, 8);
-        rooms[roomId] = [ {id: socket.id, username }];
+        rooms[roomId] = [{ id: socket.id, username }];
         socket.join(roomId);
         callback(roomId);
         io.to(roomId).emit("update-users", rooms[roomId]);
     });
 
+    // Entrar em uma sala
     socket.on("join-room", ({ roomId, username }, callback) => {
         if (rooms[roomId]) {
             rooms[roomId].push({ id: socket.id, username });
@@ -42,39 +49,23 @@ io.on("connection", (socket) => {
         }
     });
 
+    
+
+    // Receber a URL do arquivo do frontend e retransmitir para a sala
+    socket.on("send-file", ({ roomId, fileName, fileUrl, senderId }) => {
+        console.log(`Arquivo recebido na sala ${roomId}: ${fileName}`);
+        io.to(roomId).emit("receive-file", { fileName, fileUrl, senderId });
+    });
+    // Manter conexão ativa
     socket.on("keep-alive", () => {
         console.log(`Mantendo ${socket.id} ativo`);
     });
-    
 
-    socket.on("send-file-chunk", ({ roomId, chunk, fileName, last, senderId }) => {
-        if (!receivedFiles[roomId]) {
-            receivedFiles[roomId] = {};
-        }
-
-        if (!receivedFiles[roomId][fileName]) {
-            receivedFiles[roomId][fileName] = [];
-        }
-
-        // Concatenar os chunks corretamente
-        receivedFiles[roomId][fileName].push(Buffer.from(chunk));
-
-        if (last) {
-            // Montar arquivo final
-            const finalFile = Buffer.concat(receivedFiles[roomId][fileName]);
-
-            // Enviar o arquivo completo para os clientes
-            socket.to(roomId).emit("receive-file", {
-                fileName,
-                fileData: finalFile,
-                senderId
-            });
-
-            // Limpar buffer
-            delete receivedFiles[roomId][fileName];
-        }
+    socket.on("send-message", ({ roomId, message }) => {
+        io.to(roomId).emit("receive-message", message);
     });
 
+    // Lidar com desconexão
     socket.on("disconnect", () => {
         for (const roomId in rooms) {
             rooms[roomId] = rooms[roomId].filter(user => user.id !== socket.id);
